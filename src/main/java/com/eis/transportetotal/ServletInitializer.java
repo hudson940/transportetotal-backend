711e5288c05940f1d89b5c8d210f7cd8732acdc1
anderson <andersonvidal94@gmail.com>
2024-06-18 18:33:59 -0500

    [ADD] initial commit start spring project

---
+++ src/main/java/com/eis/transportetotal/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.eis.transportetotal;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(TransportetotalApplication.class);
+    }
+
+}
---
+++ src/test/java/com/eis/transportetotal/TransportetotalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eis.transportetotal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransportetotalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
